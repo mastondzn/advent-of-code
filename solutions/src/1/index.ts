8c9c63420455a6cee395ac7a04f2731268493301
Maston <MastonDZN@users.noreply.github.com>
2022-12-01 22:15:32 +0200

    add day 1 part two solution

--- solutions/src/1/index.ts
+++ solutions/src/1/index.ts
@@ -3,7 +3,7 @@ import { readFile } from 'node:fs/promises';
 const main = async () => {
     const input = await readFile('./src/1/input.txt', 'utf8');
 
-    const mostCalories = input
+    const elves = input
         .split('\n\n')
         .map((elf) => {
             const foods = elf.split('\n');
@@ -15,10 +15,12 @@ const main = async () => {
 
             return total;
         })
-        .sort()
-        .at(-1);
+        .sort();
 
-    console.log(mostCalories);
+    const mostCalories = elves.at(-1);
+    const topThreeMostCaloriesTotal = elves.slice(-3).reduce((a, b) => a + b, 0);
+
+    console.log({ mostCalories, topThreeMostCaloriesTotal });
 };
 
 void main();
