d77123c560445635b107417b49a83dd0cab751e4
Maston <MastonDZN@users.noreply.github.com>
2022-12-01 22:19:11 +0200

    add arg validation

--- solutions/src/index.ts
+++ solutions/src/index.ts
@@ -6,10 +6,14 @@ const main = async () => {
         console.log('Please provide a day (for example pnpm dev 1 or pnpm start 1)');
         return;
     }
+    if (!/^[1-9]$/.test(arg)) {
+        console.log('Please provide a valid day (for example pnpm dev 1 or pnpm start 1)');
+        return;
+    }
     const file = `./src/${arg}/index.ts`;
     const hasDay = !!(await readFile(file, 'utf8').catch(() => false));
     if (!hasDay) {
-        console.log(`File ${file} does not exist!`);
+        console.log(`File for day ${arg} does not exist!`);
         return;
     }
     await import(`./${arg}`);
