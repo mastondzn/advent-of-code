6cc9ed9a44b852af6bc1af28fc55d2a28eaa098b
Maston <MastonDZN@users.noreply.github.com>
2022-12-02 08:24:05 +0200

    start solution for day 2

---
+++ solutions/src/2/index.ts
@@ -0,0 +1,9 @@
+import { readFile } from 'node:fs/promises';
+    
+const main = async () => {
+    const input = await readFile('./src/2/input.txt', 'utf8');
+
+    // Your code here, remember to add your input file to input.txt.
+};
+
+void main();
