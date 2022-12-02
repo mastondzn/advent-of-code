e501bf089703a7129d41d95b5553f0ba99b98749
Maston <MastonDZN@users.noreply.github.com>
2022-12-02 20:02:27 +0200

    add 2021 day 3 step one solution

---
+++ solutions/src/2021/3/index.ts
@@ -0,0 +1,65 @@
+import { readFile } from 'node:fs/promises';
+
+const main = async () => {
+    const input = await readFile('./src/2021/3/input.txt', 'utf8');
+
+    const bitsColumnMap: Record<string, number> = {};
+
+    const lines = input.split('\n');
+    // eslint-disable-next-line unicorn/no-for-loop
+    for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+        const line = lines[lineIndex];
+
+        for (let columnIndex = 0; columnIndex < line.length; columnIndex++) {
+            const BIT = Number.parseInt(lines[lineIndex][columnIndex], 10);
+
+            if (!bitsColumnMap[columnIndex]) {
+                bitsColumnMap[columnIndex] = 0;
+            }
+
+            bitsColumnMap[columnIndex] += BIT;
+        }
+    }
+
+    let gammaRateAsBinaryString = '';
+    let epsilonRateAsBinaryString = '';
+    for (const [, bitTotalForColumn] of Object.entries(bitsColumnMap)) {
+        const average = bitTotalForColumn / lines.length;
+        const rounded = Math.round(average);
+
+        gammaRateAsBinaryString += rounded ? '1' : '0';
+        epsilonRateAsBinaryString += rounded ? '0' : '1';
+    }
+
+    const gammaRate = Number.parseInt(gammaRateAsBinaryString, 2);
+    const epsilonRate = Number.parseInt(epsilonRateAsBinaryString, 2);
+
+    console.log('The power consumption is', gammaRate * epsilonRate);
+
+    // step 2 pretty dank
+    // const maxColumnIndex = Math.max(
+    //     ...Object.keys(bitsColumnMap).map((key) => Number.parseInt(key, 10))
+    // );
+
+    // let oxygenSearch = [...lines];
+    // let oxygenSearchColumnIndex = 0;
+    // while (oxygenSearchColumnIndex < maxColumnIndex) {
+    //     oxygenSearch = oxygenSearch.filter((line) => {
+    //         const currentColumnRounded = Math.round(bitsColumnMap);
+
+    //         if (bitsColumnMap[oxygenSearchColumnIndex] === 0) {
+    //             return false;
+    //         }
+    //     });
+    //     oxygenSearchColumnIndex++;
+    // }
+
+    // const scrubberSearch = [...lines];
+    // let oxygenSearchColumnIndex = 0;
+
+    // Your code here, remember to add your input file to input.txt.
+    // https://adventofcode.com/2021/day/3
+    // https://adventofcode.com/2021/day/3/input
+};
+
+void main();
