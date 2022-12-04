9d3de510979b8c99c3d65145a3a41072fa078604
Maston <MastonDZN@users.noreply.github.com>
2022-12-04 11:27:28 +0200

    scaffold 2022, day 4

---
+++ solutions/src/2022/4/index.ts
@@ -0,0 +1,11 @@
+import { readFile } from 'node:fs/promises';
+    
+// https://adventofcode.com/2022/day/4
+// https://adventofcode.com/2022/day/4/input
+const main = async () => {
+    const input = await readFile('./src/2022/4/input.txt', 'utf8');
+
+    // Solve the puzzle here! Don't forget to add your input file to input.txt.
+};
+
+void main();
