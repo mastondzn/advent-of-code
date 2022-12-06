c3d070422547b3ae5ff8fe0804ae6be22f0c8d18
Maston <MastonDZN@users.noreply.github.com>
2022-12-06 13:23:57 +0200

    add 2022 day 6 solution

--- solutions/src/2022/6/index.ts
+++ solutions/src/2022/6/index.ts
@@ -1,5 +1,26 @@
 // https://adventofcode.com/2022/day/6
 // https://adventofcode.com/2022/day/6/input
 export const solution = (file: string): void => {
-    // Solve the puzzle here! Don't forget to add your input file to input.txt.
+    const getProcessedCount = (uniqueCharactersNeeded: number): number => {
+        let brokeOut = false;
+        let current = '';
+        let processed: number;
+        for (processed = 0; processed < file.length; processed++) {
+            const character = file[processed];
+            current += character;
+            if (current.length < uniqueCharactersNeeded) continue;
+
+            const last = current.slice(-uniqueCharactersNeeded);
+            const unique = new Set(last);
+            if (unique.size === uniqueCharactersNeeded) {
+                brokeOut = true;
+                break;
+            }
+        }
+        if (!brokeOut) throw new Error('No solution found');
+        return processed + 1;
+    };
+
+    console.log(getProcessedCount(4), 'characters need to be processed for start-of-packet');
+    console.log(getProcessedCount(14), 'characters need to be processed for start-of-message');
 };
