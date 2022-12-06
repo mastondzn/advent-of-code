720ed72d7602fcd76e32ca7dead512057f388664
Maston <MastonDZN@users.noreply.github.com>
2022-12-06 12:17:48 +0200

    scaffold 2022 day 6

---
+++ solutions/src/2022/6/index.ts
@@ -0,0 +1,5 @@
+// https://adventofcode.com/2022/day/6
+// https://adventofcode.com/2022/day/6/input
+export const solution = (file: string): void => {
+    // Solve the puzzle here! Don't forget to add your input file to input.txt.
+};
