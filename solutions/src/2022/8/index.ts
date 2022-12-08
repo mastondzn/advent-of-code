5895e3908a02b5828f05c52eebe1e3d4edeb9bbf
Maston <MastonDZN@users.noreply.github.com>
2022-12-08 14:04:58 +0200

    scaffold 2022 day 8

---
+++ solutions/src/2022/8/index.ts
@@ -0,0 +1,5 @@
+// https://adventofcode.com/2022/day/8
+// https://adventofcode.com/2022/day/8/input
+export const solution = (file: string): void => {
+    // Solve the puzzle here! Don't forget to add your input file to input.txt.
+};
